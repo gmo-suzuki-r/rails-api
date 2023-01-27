683e73bbe1e410462a5aae16660d9e51444dbbc7
Suzuki Ren <suzuki-r@gmo-connect.jp>
2023-01-27 17:06:31 +0900

    Authentication service failure

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    sequence(:login) { |n| "User #{n}" }
+    sequence(:login) { |n| "jsmith #{n}" }
     name { "John Smith" }
     url { "http://example.com" }
     avatar_url { "http://example.com/avatar" }
